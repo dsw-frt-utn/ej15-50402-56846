ea99f4e878a49e820e8f550ebfc344eb4ac351d8
FacuPalacios <facupalacios21@gmail.com>
2026-06-18 18:27:50 -0300

    Se crearon las entidades del Dominio

---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; init; }
+
+        protected BaseEntity(Guid? Id = null)
+        {
+            Id = Id ?? Guid.NewGuid();
+        }
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Entities/Speciality.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    internal class Speciality : BaseEntity
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+
+        public Speciality(string name, string description, Guid? id = null) : base(id)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
